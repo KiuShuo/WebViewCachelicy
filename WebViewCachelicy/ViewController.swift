2b42fa4172dcfe59b0f89cd4d3833cd2ef74b551
KiuShuo <emliushuo@163.com>
2017-02-15 14:35:26 +0800

    优化

--- WebViewCachelicy/ViewController.swift
+++ WebViewCachelicy/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         if isClear {
             clearCacheBt.setTitle("clearCache", for: .normal)
         } else {
